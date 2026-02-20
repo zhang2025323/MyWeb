9cf7b5656f710eef9429d97ddeb3b79a9c4fa974
zhang2025323 <2419771662@qq.com>
2026-02-20 15:47:33 +0800

    Configure publicPath in vue.config.js
    Set publicPath for deployment to GitHub Pages.

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  // 关键：末尾的斜杠不能少，仓库名大小写要和 GitHub 一致
+  publicPath: '/MyWeb/' 
+}
