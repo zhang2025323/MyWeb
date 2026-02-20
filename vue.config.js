70dd6d6dd0391cd15bb013e5ceae8b55a156d1ab
zhang2025323 <2419771662@qq.com>
2026-02-20 16:05:07 +0800

    Fix syntax errors in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-const   常量   常量 { const   常量 {defineConfig} = require（'@vue/cli-service'   “@vue / cli-service”）defineConfig } = require   需要(   “@vue / cli-service”'@vue/cli   “@vue / cli-service”-service'   需要)
-module      模块模块.exports   出口   出口 = defineConfig({
-  transpileDependencies: true   真正的   真正的,
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
   // 关键配置：和 GitHub 仓库名 MyWeb 完全一致（大小写、斜杠都要对）
   publicPath: '/MyWeb/'
 })
