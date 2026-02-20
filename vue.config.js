0e7561d66f50540fb4ba03fd87358c8c1a3671d8
zhang2025323 <2419771662@qq.com>
2026-02-20 16:02:11 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+const   常量   常量 { const   常量 {defineConfig} = require（'@vue/cli-service'   “@vue / cli-service”）defineConfig } = require   需要(   “@vue / cli-service”'@vue/cli   “@vue / cli-service”-service'   需要)
+module      模块模块.exports   出口   出口 = defineConfig({
+  transpileDependencies: true   真正的   真正的,
+  // 关键配置：和 GitHub 仓库名 MyWeb 完全一致（大小写、斜杠都要对）
+  publicPath: '/MyWeb/'
 })
-
-module.exports = {
-  // 关键：末尾的斜杠不能少，仓库名大小写要和 GitHub 一致
-  publicPath: '/MyWeb/' 
-}
