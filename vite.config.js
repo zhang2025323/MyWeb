1c9b2c0fff023be19ab9eb4829d82744315ba157
zhangwen202597 <2419771662@qq.com>
2026-03-14 10:13:02 +0800

    提交 MyWeb 项目打包文件

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   // 关键：配置公共路径（部署到 Gitee 必须加仓库名）
-  base: '/your-gitee-repo-name/', // 替换成你的 Gitee 仓库名，比如 /vue3-demo/
+  base: '/MyWeb/', // 替换成你的 Gitee 仓库名，比如 /vue3-demo/
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
